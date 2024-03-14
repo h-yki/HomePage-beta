d93b7078290bf131b2775224583e893bc24a555e
h-yki <yuuki531259@gmail.com>
2024-03-15 04:40:50 +0900

    試作ヘッダー

--- src/header.jsx
+++ src/header.jsx
@@ -1,84 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import './styles/header.css'; // CSSファイル名を変更
+import React, { useState } from 'react';
+import { Layout, Menu, Drawer, Button } from 'antd';
+import { MenuOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive'; 
 
-const Header = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
-  const [menuOpen, setMenuOpen] = useState(false);
+const Header = Layout;
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+const MainHeader = () => {
+  const [selectkey, setSelectkey] = useState(['0']);
+  const isMobile = useMediaQuery({ maxWidth: 800 });
+  const [visible, setVisible] = useState(false);
+
+  const handlekey = () =>{
+    setSelectkey(['0']);
+  }
+
+  const showDrawer = () =>{
+    setVisible(true);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 800);
-      setMenuOpen(false);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  const onClose = () => {
+    setVisible(false);
+  };
 
   return (
-    <div className={`container ${isMobile ? 'mobile' : 'desktop'}`}>
-      <div className="header">
-        <div className="logo">
-          <a href="/" style={{ color: 'white', textDecoration: 'none' }}>
-            yuumal.dev
-          </a>
+    <div>
+      {!isMobile ? (
+        <div>
+          <Layout>
+            <Header
+              style={{
+                display: 'flex',
+                position: "fixed",
+                top: 0,
+                zIndex: 1,
+                width: "100%",
+                height: '50px',
+                justifyContent:'space-between',
+                alignItems:"center",
+              }}
+            >
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                style={{ minWidth: 0 , width:"100%"}}
+                selectedKeys={selectkey}
+                onSelect={(item) => setSelectkey([item.key])}
+              >
+                <div className="logo" style={{justifyContent:"left"}}>
+                  <Link to='/' style={{ color: 'white', textDecoration: 'none', marginLeft:'10px', fontSize:"15px", marginRight:"10px" }} onClick={handlekey}>yuumal.dev</Link>
+                </div>
+                <Menu.Item key="1">
+                  <Link to="/About">About</Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <Link to="/eventlog">EventLog</Link>
+                </Menu.Item>
+                <Menu.Item key="3">
+                  <Link to="/contact">Contact</Link>
+                </Menu.Item>
+              </Menu>
+            </Header>
+          </Layout>
+        </div> 
+        ):(
+        <div>
+          <Layout>
+            <Header
+             style={{
+              display: 'flex',
+              position: "fixed",
+              top: 0,
+              zIndex: 1,
+              width: "100%",
+              height: '50px',
+              justifyContent:'space-between',
+              alignItems:"center",
+            }}>
+              <div style={{display:'flex', backgroundColor:'#111d2c', width:'100%', flex:'1'}}>
+              <div className="logo" style={{justifyContent:"left", marginTop:'13px'}}>
+                <Link to='/' style={{ color: 'white', textDecoration: 'none', marginLeft:'10px', fontSize:"15px", marginRight:"15px"}}>yuumal.dev</Link>
+              </div>
+                <Button icon={<MenuOutlined/>} onClick={showDrawer} style={{float:'right'}}/>
+                <Drawer title='yuumal.dev' placement='right' onClose={onClose} open={visible} width='50%' style={{backgroundColor:'#111d2c', color:'white'}}>
+                  <p><Link to="/About" onClick={onClose} style={{color:'white', fontSize:'20px'}}>About</Link></p>
+                  <p><Link to="/eventlog" onClick={onClose} style={{color:'white', fontSize:'20px'}}>EventLog</Link></p>
+                  <p><Link to="/contact" onClick={onClose} style={{color:'white', fontSize:'20px'}}>Contact</Link></p>
+                </Drawer>
+              </div>
+            </Header>
+          </Layout>
         </div>
-        {isMobile ? (
-          <div
-            className={`mobile-menu hamburger-menu ${menuOpen ? 'active' : ''}`}
-            onClick={toggleMenu}
-          >
-            <div className="line"></div>
-            <div className="line"></div>
-            <div className="line"></div>
-          </div>
-        ) : (
-          <nav className={`pc-menu ${menuOpen ? 'active' : ''}`}>
-            <ul>
-              <li>
-                <a href="/about" style={{ color: 'white' }}>
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="/Eventlog" style={{ color: 'white' }}>
-                  Eventlog
-                </a>
-              </li>
-              <li>
-                <a href="/contact" style={{ color: 'white' }}>
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </nav>
-        )}
-      </div>
-      {isMobile && (
-        <nav className={`nav-menu ${menuOpen ? 'active' : ''}`}>
-          <a href="/" className="menu-link" style={{ color: 'white' }}>
-            Home
-          </a>
-          <a href="/about" className="menu-link" style={{ color: 'white' }}>
-            About
-          </a>
-          <a href="/Eventlog" className="menu-link" style={{ color: 'white' }}>
-            Eventlog
-          </a>
-          <a href="/contact" className="menu-link" style={{ color: 'white' }}>
-            Contact
-          </a>
-        </nav>
       )}
     </div>
   );
 };
 
-export default Header;
+export default MainHeader;
