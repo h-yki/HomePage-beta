6e4ca0b9321da46b584c445303013bbd0ebc5dd2
h-yki <yuuki531259@gmail.com>
2025-04-21 09:55:21 +0900

    Readme変更

--- src/Contents/Works.jsx
+++ src/Contents/Works.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Works = () => {
-    return (
-        <div>
-            <h2>503 Service Unavailable</h2>
-            <p>ただいまこのページは開発中のためご利用いただけません。</p>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Works</h2>
+      <h3>…開発中…</h3>
+      <p>ただいまこのページは開発中のためご利用いただけません。</p>
+    </div>
+  );
 };
 
 export default Works;
