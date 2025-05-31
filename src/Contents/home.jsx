bbf822fe45f805f07c319fb936b0fb837bd2ba51
h-yki <yuuki531259@gmail.com>
2025-05-31 21:49:14 +0900

    homeに表示するアカウントの変更

--- src/Contents/home.jsx
+++ src/Contents/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className='container'>
       <p>情報科学を専攻し、女性声優音楽を好んでいます</p>
       <ul className='follow'>
-        <li><a href='https://x.com/wv_hmd' target="_blank" rel="noreferrer"></a></li>
+        <li><a href='https://x.com/mald_y' target="_blank" rel="noreferrer"></a></li>
         <li><a href='https://github.com/h-yki' target="_blank" rel="noreferrer"></a></li>
       </ul>
     </div>
